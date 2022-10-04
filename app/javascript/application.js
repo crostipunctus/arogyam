2387868097056d932c6acc1bcc30f30ec85dde68
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2022-10-04 10:10:07 +0530

    dropdown arrows fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,39 @@
 // Entry point for the build script in your package.json
+
+//= require swiper
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
 
-import "./theme.js"
-import "./customizer.js"
-import "./lightgallery.js"
-import "./swiper-bundle.js"
-import "./main.js"
-import "./locales-all.js"
 
+import "./theme"
+import "./customizer"
+// import "./lightgallery.js"
+import "./swiper-bundle"
+// import "./postinstall"
+// import "./main.js"
+// import "./locales-all.js"
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
