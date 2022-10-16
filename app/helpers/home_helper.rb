16c69ad08b478c2733cc8af7d00153039e0cb1f1
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2022-10-16 11:42:43 +0530

    home text partial

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,5 @@
 module HomeHelper
 
 
-  def home_text 
-    "We are witnessing a shift in the concept of health away from the primary approach of  diseases and their management and the embracing of wellbeing.
-    At ArogyaM, we explore the concept of wellness through four programs:
-    - SwaasthyaM (Detoxification regimen)-10 days
-    - ShaantaM (De-stress)-10 days
-    - LanghanaM (Weight management)-15 days
-    - VishraaM (Rest and Rejuvenate)-3 days
-    Every Wellness Program integrates Yoga and Ayurveda through traditional treatments and practice of Asanas and Pranayama. The study of Ashtanga Yoga and unveiling Ayurveda with the help of traditional texts add a unique flavour and a greater understanding of body and mind health and their integration.
-    The Sacred Grove which houses ArogyaM provides the natural setting most condusive for the healing of body and mind and exploring the spiritual dimension which is one's true nature. The vast stretches of space and silence and the well-stocked library allows study and contemplation on the body-mind-soul complex that is a human being (sattwa-atma-shariram ca trayametat tridandavat)."
-  end 
+
 end
