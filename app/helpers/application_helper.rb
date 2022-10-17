6228e208ebb949f7eeb56ea104cbc15c137e966a
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2022-10-17 21:50:18 +0530

    layout finetuning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def logo 
-    link_to image_tag('treeicon.png', height: 70, width: 70), root_path, data: {turbo: false}
+    link_to image_tag('treeicon.png', height: 60, width: 60), root_path, data: {turbo: false}
   end 
 
  
