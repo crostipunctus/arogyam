96819d8eb04811b27053377bb258b1a9cb3745d0
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2022-10-23 11:40:26 +0530

    reverted to earlier link fix

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,8 @@
 module HomeHelper
 
   def package_link(name)
-    
     package = Package.find_by(name: name)
- 
-    link_to "#{name}", package_path(package.id)
+    link_to "#{name}", package_path(package)
   end
 
 end
