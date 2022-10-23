7fc71eb0ae48f56cc5322a6f33faec64b5922510
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2022-10-23 11:46:09 +0530

    moved helper to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,10 @@ module ApplicationHelper
     link_to image_tag('treeicon.png', height: 60, width: 60), root_path, data: {turbo: false}
   end 
 
+  def package_link(name)
+    package = Package.find_by(name: name)
+    link_to "#{name}", package_path(package)
+  end
+
  
 end
