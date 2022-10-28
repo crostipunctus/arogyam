a442ed9f8cad7b5d486c0f29179162b2114cb5e5
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2022-10-28 18:57:16 +0530

    added passenger_restart to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,3 +49,4 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+set :passenger_restart_with_touch, true
