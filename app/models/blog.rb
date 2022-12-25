039acb7be4958d4c6d6342de8be3deb2d1ffe5f5
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2022-12-25 10:42:18 -0600

    added blog show page

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  has_rich_text :title
+  has_rich_text :content 
 
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 25 } 
