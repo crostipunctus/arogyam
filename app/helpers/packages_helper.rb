64af5835fd821418f1b208fd970dd9ba38db9dcb
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-02-07 09:10:02 +0530

    trying cloudfront for package_image

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -3,7 +3,7 @@ module PackagesHelper
 
   def package_image(package)
     if package.package_image.attached?
-      image_tag package.package_image
+      image_tag package.package_image, src: "https://d1w11gv0j27jrz.cloudfront.net#{rails_blob_path(package.package_image, disposition: "attachment", only_path: true)}"
     else
       image_tag "flowers.jpg"
     end
