a09febb0c84884936db349f2b14e84d8dcdba879
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-02-15 11:15:21 +0530

    contact validations added

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true 
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :message, presence: true, length: { minimum: 10, maximum: 200}
 end
