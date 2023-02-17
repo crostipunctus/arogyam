6f7833dc26338521fe67d8b0fe69e571e284bb96
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-02-17 10:17:09 +0530

    dropshadows and some minor adjustments

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -1,7 +1,7 @@
 module PackagesHelper
   def package_image(package)
     if package.package_image.attached?
-      image_tag package.package_image, src: "https://d1w11gv0j27jrz.cloudfront.net#{rails_blob_path(package.package_image, disposition: "attachment", only_path: true)}"
+      image_tag package.package_image, src: "https://d1w11gv0j27jrz.cloudfront.net#{rails_blob_path(package.package_image, disposition: "attachment", only_path: true)}", class: "PackImage"
     else
       image_tag "flowers.jpg"
     end
