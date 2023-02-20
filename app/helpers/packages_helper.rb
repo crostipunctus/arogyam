ad8b36cf8029c00f75de15d82e1838b0b109632f
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-02-20 15:55:00 +0530

    added alt

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -1,9 +1,9 @@
 module PackagesHelper
   def package_image(package)
     if package.package_image.attached?
-      image_tag(rails_public_blob_url(package.package_image), class: "PackImage")
+      image_tag(rails_public_blob_url(package.package_image), class: "PackImage", alt: 'package-image')
     else
-      image_tag "flowers.jpg"
+      image_tag "flowers.jpg", alt: 'flower-placeholder'
     end
   end
 end
