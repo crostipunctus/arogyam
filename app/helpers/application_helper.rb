bd4aa174c6efba49763677d983c37c03f5280e06
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-02-20 16:01:10 +0530

    added alt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,10 @@
 module ApplicationHelper
 
   def logo 
-    link_to image_tag('https://d1w11gv0j27jrz.cloudfront.net/symbol.png', class: "no-border-radius", width: 20), root_path, data: {turbo: false}
+    link_to image_tag('https://d1w11gv0j27jrz.cloudfront.net/symbol.png', class: "no-border-radius", width: 20, alt: 'symbol'), root_path, data: {turbo: false}
   end 
 
-  def package_link(name)
-    package = Package.find_by(name: name)
-    link_to "#{name}", package_path(package)
-  end
+ 
 
   def get_current_url
     request.original_url
