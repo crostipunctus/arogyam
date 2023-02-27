a3c2b25208fec65fe818245d26efaec8e0c91350
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-02-27 12:41:08 +0530

    removed presence validation for message in booking form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   validates :name, presence: true 
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-  validates :message, presence: true, length: { minimum: 10, maximum: 200}
+  validates :message, length: { maximum: 200}
 
   belongs_to :package
 end
