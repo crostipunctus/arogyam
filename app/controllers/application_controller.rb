0710aa6b34cbbc3a806779981171ad1f3d2ea606
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-03-23 09:30:13 +0530

    register for batch working but register for users not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,29 @@ class ApplicationController < ActionController::Base
   helper_method :gallery_index
   helper_method :team_index
   helper_method :announcements
-  
+  protect_from_forgery with: :exception
+
+before_action :store_user_location!, if: :storable_location?
+
+def after_sign_in_path_for(resource_or_scope)
+
+stored_location_for(resource_or_scope) || super
+
+end
+
+private
+
+def storable_location?
+
+request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+
+end
+
+def store_user_location!
+
+store_location_for(:user, request.fullpath)
+
+end
 
   def require_admin
     unless current_user_admin?
