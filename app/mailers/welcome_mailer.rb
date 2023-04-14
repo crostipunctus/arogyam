af4175411814df9fd5ae888760f7a548590bf4da
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-04-14 14:07:29 +0530

    email style

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -3,10 +3,13 @@ class WelcomeMailer < ApplicationMailer
   include SendGrid
 
   def welcome_email(user)
+    @user = user
     from = Email.new(email: 'ArogyaM@arogyam.life')
     to = Email.new(email: user.email)
     subject = 'Welcome to ArogyaM'
-    content = Content.new(type: 'text/plain', value: 'Thank you for signing up! We hope you find our services useful. Please feel free to contact us at wellnesscenter@satsang-foundation.org')
+    html_content = render_to_string(template: 'welcome_mailer/welcome_email')
+
+    content = Content.new(type: 'text/html', value: html_content)
     mail = Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: SENDGRID_API_KEY)
     response = sg.client.mail._('send').post(request_body: mail.to_json)
