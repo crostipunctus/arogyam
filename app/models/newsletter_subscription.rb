c71941b2231408cf112bf3bce3b8ce4bd581081e
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-04-19 15:04:26 +0530

    style

--- app/models/newsletter_subscription.rb
+++ app/models/newsletter_subscription.rb
@@ -2,7 +2,6 @@ class NewsletterSubscription < ApplicationRecord
   validates :email, presence: true, uniqueness: true,
     format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 
-  #after_create :subscribe_to_mailchimp
-
+  
  
 end
