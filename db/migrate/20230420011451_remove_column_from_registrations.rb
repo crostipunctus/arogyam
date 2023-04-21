d234b2a1fa8630b94714cd78df33b06e26c787ef
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-04-21 10:58:20 +0530

    removed 2 migration files

--- db/migrate/20230420011451_remove_column_from_registrations.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromRegistrations < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :registrations, :status, :string
-  end
-end
--- db/migrate/20230420011603_remove_column_from_vishraam_registrations.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromVishraamRegistrations < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :vishraam_registrations, :status, :string
-  end
-end
