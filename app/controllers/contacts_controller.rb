a4ec5ed94a8c9ed82359ff88c3764e2aef81cb5f
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-04-23 12:11:19 +0530

    captcha only on contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,6 +9,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     recaptcha_token = params[:recaptcha_token]
     recaptcha_success = verify_recaptcha(secret_key: Rails.application.credentials.recaptcha[:secret_key], response: recaptcha_token, action: 'contact')
+    Rails.logger.info "reCAPTCHA response: #{recaptcha_reply.inspect}"
     if recaptcha_success && @contact.save
       ContactMailer.contact_email(@contact).deliver_later
       redirect_to contacts_path, notice: "Your message has been sent. We will get back to you soon."
