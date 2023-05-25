7a10abc190853aeb1a21cc481fba06524666fbd0
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-05-25 19:07:40 +0530

    deploy.rb unintentional edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # Default deploy_to directory is /var/www/my_app_name
 
 
+
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
