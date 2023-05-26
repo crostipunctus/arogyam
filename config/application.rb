4db8f23b1b969a595e0227cca54870942f54814d
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-05-26 07:03:34 +0530

    removed sidekiq

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ module Arogyam
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.active_job.queue_adapter = :sidekiq
-
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
