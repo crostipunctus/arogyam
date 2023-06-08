5b671838a298be6a2174c13b3a537d7e03e43135
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-06-08 19:02:35 +0530

    trying websocket broadcasts

--- app/models/online_consultation.rb
+++ app/models/online_consultation.rb
@@ -3,13 +3,13 @@ class OnlineConsultation < ApplicationRecord
   belongs_to :booking_date
   has_many :case_sheets
 
+ 
+
   def confirmed? 
     status == "confirmed"
   end 
 
-  def review_consultation(online_consultation)
-    
-  end
+  
 
 
 end 
