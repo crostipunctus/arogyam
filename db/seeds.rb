8407e58e2ce7eea20486fc28a6789a026f901115
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-08-11 12:34:27 +0530

    all registrations showing

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,15 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 date_ranges = [
-  ["2023-04-15", "2023-04-24"],
-  ["2023-05-01", "2023-05-15"],
-  ["2023-05-21", "2023-05-30"],
-  ["2023-06-06", "2023-06-20"],
-  ["2023-07-06", "2023-07-15"],
-  ["2023-07-21", "2023-08-04"],
-  ["2023-08-11", "2023-08-20"],
+  ["2024-04-15", "2024-04-24"],
+  ["2024-05-01", "2024-05-15"],
+  ["2024-05-21", "2024-05-30"],
+  ["2024-06-06", "2024-06-20"],
+  ["2024-07-06", "2024-07-15"],
+  ["2024-07-21", "2024-08-04"],
+  ["2024-08-11", "2024-08-20"],
 ]
+
 
 # Clear existing data to avoid duplicates when seeding
 Batch.destroy_all
@@ -29,3 +30,5 @@ date_ranges.each do |start_date, end_date|
 end
 
 # Create Package records
+Package.create!(name: "VishraM", short_description: "Rest and rejuvenation", duration: "10 days", cost: "10000")  
+Package.create!(name: "ShamanM", short_description: "Rest and rejuvenation", duration: "5 days", cost: "50000")
