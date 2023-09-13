23661e0cf1775849b30defc010cd907e6eff8e2c
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-09-13 17:06:09 +0530

    blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :current_user_admin?, only: [:new, :edit, :destroy]
 
   def index 
