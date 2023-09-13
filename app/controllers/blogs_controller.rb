286a6e081c400165fd93a2ccca0f2f0da4a8c73a
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-09-13 17:34:20 +0530

    blog image

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -53,7 +53,7 @@ class BlogsController < ApplicationController
   private 
 
   def blog_params 
-    params.require(:blog).permit(:title, :content)
+    params.require(:blog).permit(:title, :content, :image)
   end 
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [500, 500]
+  end
   has_rich_text :content 
   validates :title, presence: true
   validates :content, presence: true
