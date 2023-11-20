d3df1212ea90bee58a42adc7035004d7abe0d806
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-11-20 11:31:35 +0530

    paginate working

--- Gemfile
+++ Gemfile
@@ -96,6 +96,8 @@ gem "sentry-ruby"
 
 gem "sentry-rails"
 
+gem 'faker'
+
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
 
 date_ranges = [
   ["2024-04-15", "2024-04-24"],
@@ -21,6 +22,10 @@ date_ranges = [
 Batch.destroy_all
 Package.destroy_all
 
+
+User.destroy_all
+
+
 # Iterate through the date ranges and create Batch records
 date_ranges.each do |start_date, end_date|
   Batch.create!(
@@ -31,4 +36,23 @@ end
 
 # Create Package records
 Package.create!(name: "VishraM", short_description: "Rest and rejuvenation", duration: "10 days", cost: "10000")  
-Package.create!(name: "ShamanM", short_description: "Rest and rejuvenation", duration: "5 days", cost: "50000")
+Package.create!(name: "ShamanM", short_description: "Rest and rejuvenation", duration: "5 days", cost: "50000") 
+
+number_of_users = 50
+
+# Define a default password for all dummy users
+default_password = 'password123'
+
+# Clear existing users to avoid duplicates when seeding
+
+
+number_of_users.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: default_password,
+    password_confirmation: default_password,
+    confirmed_at: Time.now, # Confirm the user to satisfy Devise confirmable
+    first_name: Faker::Name.first_name, # Generate a first name
+    last_name: Faker::Name.last_name    # Generate a last name
+  )
+end
