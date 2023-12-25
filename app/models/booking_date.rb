7520cea7d0d6d741ffb11d4e15463fa2b6edf8b2
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2023-12-25 10:42:54 +0530

    removed all booking dates

--- app/models/booking_date.rb
+++ app/models/booking_date.rb
@@ -5,48 +5,48 @@ class BookingDate < ApplicationRecord
  
   
   
-  def self.generate_default_slots 
-    (0..29).each do |days_from_now|
-      date = Date.current + days_from_now.days
-      next if date.sunday?
+  # def self.generate_default_slots 
+  #   (0..29).each do |days_from_now|
+  #     date = Date.current + days_from_now.days
+  #     next if date.sunday?
   
-      slot_times = [["14:00", "14:30"], ["15:00", "15:30"]] # 2 PM to 2:30 PM and 3 PM to 3:30 PM
+  #     slot_times = [["14:00", "14:30"], ["15:00", "15:30"]] # 2 PM to 2:30 PM and 3 PM to 3:30 PM
   
-      slot_times.each do |slot|
-        BookingDate.create!(
-          date: date,
-          start_time: slot[0], 
-          end_time: slot[1],
-          available: true
-        )
-      end
-    end
+  #     slot_times.each do |slot|
+  #       BookingDate.create!(
+  #         date: date,
+  #         start_time: slot[0], 
+  #         end_time: slot[1],
+  #         available: true
+  #       )
+  #     end
+  #   end
 
-  #  AddDayWorker.perform_async unless already_queued? 
-   end
+  # #  AddDayWorker.perform_async unless already_queued?  
+  #  end
   
  
   
 
-  def self.generate_day_slots(date)
-    return if date.sunday?  
+  # def self.generate_day_slots(date)
+  #   return if date.sunday?  
   
-    slot_times = [["14:00", "14:30"], ["15:00", "15:30"]] # 2 PM to 2:30 PM and 3 PM to 3:30 PM
+  #   slot_times = [["14:00", "14:30"], ["15:00", "15:30"]] # 2 PM to 2:30 PM and 3 PM to 3:30 PM
   
-    slot_times.each do |slot|
-      BookingDate.create!(
-        date: date,
-        start_time: slot[0],
-        end_time: slot[1],
-        available: true
-      )
-    end
-  end
+  #   slot_times.each do |slot|
+  #     BookingDate.create!(
+  #       date: date,
+  #       start_time: slot[0],
+  #       end_time: slot[1],
+  #       available: true
+  #     )
+  #   end
+  # end
 
-  private 
+  # private 
 
-  def self.already_queued?
-    Sidekiq::Queue.new.any? { |job| job.klass == 'AddDayWorker' }
-  end
+  # def self.already_queued?
+  #   Sidekiq::Queue.new.any? { |job| job.klass == 'AddDayWorker' }
+  # end
 
 end
--- app/workers/add_day_worker.rb
+++ app/workers/add_day_worker.rb
@@ -1,11 +1,11 @@
 class AddDayWorker
   include Sidekiq::Worker
 
-  def perform
-    # Add a day of slots
-    BookingDate.generate_day_slots(Date.current + 30.days)
+  # def perform
+  #   # Add a day of slots
+  #   BookingDate.generate_day_slots(Date.current + 30.days)
 
-    # Schedule this worker to run again in 24 hours
-    AddDayWorker.perform_in(24.hours) unless BookingDate.already_queued?
-  end
+  #   # Schedule this worker to run again in 24 hours
+  #   # AddDayWorker.perform_in(24.hours) unless BookingDate.already_queued?
+  # end
 end
